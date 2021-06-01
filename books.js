38004d9883a2c585f814d078b9b80a6dfb2963ed
Jared Hammond <jared@jaredhammond.me>
2021-06-01 14:46:31 -0400

    Added function for adding book objects to array. Built basic framework to display book object contents

--- books.js
+++ books.js
@@ -1,13 +1,32 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
-    this.title = "";
-    this.author = "";
-    this.pages = 0;
-    this.read = false;
-    this.info = function() {
-        const haveRead = read ? 'have read book' : 'have not read book';
-        return title + ' by ' + author + ', ' + pages + ' pages, ' + haveRead + '.';
-    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 456, true);
-console.log(theHobbit.info());
+function addToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+addToLibrary('The Hobbit', 'J.R.R. Tolkien', 456, false);
+addToLibrary('The Giver', 'Some Guy', 100, true);
+console.log(myLibrary);
+
+const cardArea = document.getElementById('card-area');
+
+myLibrary.forEach(book => {
+    const card = document.createElement('div');
+    card.classList.add('book-card');
+    
+    const cardTitle = document.createElement('p');
+    cardTitle.innerText = "Title: " + book.title;
+    card.appendChild(cardTitle);
+
+    const cardAuthor = docuemnt.createElement('p');
+    cardAuthor
+
+    cardArea.appendChild(card);
+})
