6bbce58e57dfcde484e9e81d6b12baa1e5ad59a5
Jared Hammond <jared@jaredhammond.me>
2021-06-01 22:04:39 -0400

    minor progress. Saving for future work

--- books.js
+++ books.js
@@ -1,32 +1,87 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
-    this.read = read;
 }
 
-function addToLibrary(title, author, pages, read) {
-    let newBook = new Book(title, author, pages, read);
+function addToLibrary(title, author) {
+    let newBook = new Book(title, author);
     myLibrary.push(newBook);
 }
-addToLibrary('The Hobbit', 'J.R.R. Tolkien', 456, false);
-addToLibrary('The Giver', 'Some Guy', 100, true);
-console.log(myLibrary);
+
+function setAttributes(el, attr) {
+    for (key in attr) {
+        el.setAttribute(key, attr[key]);
+    }
+}
+
+function submitBook() {
+    const newTitle = titleBox.value;
+    const newAuthor = authorBox.value;
+
+    addToLibrary(newTitle, newAuthor);
+}
+
+function showForm() {
+    document.body.removeChild(newBookButton);
+    const newBookForm = document.createElement('form');
+
+    const titleLabel = document.createElement('label');
+    titleLabel.setAttribute('for','title');
+    titleLabel.innerText = 'Title';
+    newBookForm.appendChild(titleLabel);
+    
+    const titleBox = document.createElement('input');
+    setAttributes(titleBox, {'name':'title', 'type':'text', 'required':''});
+    newBookForm.appendChild(titleBox);
+
+    const authorLabel = document.createElement('label');
+    authorLabel.setAttribute('for','author');
+    authorLabel.innerText = 'Author';
+    newBookForm.appendChild(authorLabel);
+
+    const authorBox = document.createElement('input');
+    setAttributes(authorBox, {'name':'author', 'type':'text', 'required':''});
+    newBookForm.appendChild(authorBox);
+    
+    cardArea.appendChild(newBookForm);
+
+    const submitButton = document.createElement('button');
+    submitButton.innerText = 'Add Book';
+    submitButton.addEventListener('click', submitBook);
+    cardArea.appendChild(submitButton);
+}
+
 
 const cardArea = document.getElementById('card-area');
 
-myLibrary.forEach(book => {
-    const card = document.createElement('div');
-    card.classList.add('book-card');
-    
-    const cardTitle = document.createElement('p');
-    cardTitle.innerText = "Title: " + book.title;
-    card.appendChild(cardTitle);
+function initialize() {
+        myLibrary.forEach(book => {
+        const card = document.createElement('div');
+        card.classList.add('book-card');
+        
+        const cardTitle = document.createElement('p');
+        cardTitle.innerText = "Title: " + book.title;
+        card.appendChild(cardTitle);
+        
+        const cardAuthor = document.createElement('p');
+        cardAuthor.innerText = 'By: ' + book.author;
+        card.appendChild(cardAuthor);
+        
+        const cardRead = document.createElement('p');
+        book.read ? cardRead.innerText = 'Yes' : cardRead.innerText = 'No'
+        card.appendChild(cardRead);
+        
+        cardArea.appendChild(card);
+    })
+}
 
-    const cardAuthor = docuemnt.createElement('p');
-    cardAuthor
 
-    cardArea.appendChild(card);
-})
+const newBookButton = document.getElementById('new-book');
+newBookButton.addEventListener('click', showForm);
+
+
+addToLibrary('The Hobbit', 'J.R.R. Tolkien', 456, false);
+addToLibrary('The Giver', 'Some Guy', 100, true);
+initialize();
